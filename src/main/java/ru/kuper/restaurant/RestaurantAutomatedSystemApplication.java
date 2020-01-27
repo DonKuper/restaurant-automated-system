c6e9b844cdbba9a0ee587190c1233c54f25fd743
Ilya Kupriyanov <kup-92@yandex.ru>
2020-01-27 18:52:28 +0300

    initial commit

---
+++ src/main/java/ru/kuper/restaurant/RestaurantAutomatedSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.kuper.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantAutomatedSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantAutomatedSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kuper/restaurant/RestaurantAutomatedSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuper.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAutomatedSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
